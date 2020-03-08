a031b1c572a828f6da7e2b35ef006a2a1d756030
angelvinyals <angel.vinyals@gmail.com>
2020-03-08 19:10:10 +0100

    ADD some components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Card, CardBody, CardTitle, Button, Jumbotron, Fade, CardText } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fadeIn: false };
+    this.toggle = this.toggle.bind(this);
+  }
 
-export default App;
+  toggle() {
+    this.setState({
+      fadeIn: !this.state.fadeIn
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-2" />
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+          </p>
+        </Jumbotron>
+        <Card>
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <Button color="primary" onClick={this.toggle}>Magic</Button>
+            <Fade in={this.state.fadeIn} className='my-2'>
+              <CardText>Fading text</CardText>
+            </Fade>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+ 
+};
